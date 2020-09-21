411fa24856f5551ace4f0c0888ed528e83d0c8b7
bhumika-bhatt-au7 <bhumikabhatt711@gmail.com>
2020-09-21 18:08:35 +0530

    login-frontend

---
+++ client/src/components/UI/Navbar/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import Logo from "../../../assets/images/logo.png";
+import classes from "./Navbar.module.css";
+
+const navbar = (props) => {
+  return (
+    <nav>
+      <div class="nav-wrapper">
+        <NavLink to="/" exact class="brand-logo">
+          <img className={classes.Logo} src={Logo} alt="connectX" />
+        </NavLink>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li>
+            <NavLink to="/">ConnectX</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login">Login</NavLink>
+          </li>
+          <li>
+            <NavLink to="/signup">SignUp</NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default navbar;
