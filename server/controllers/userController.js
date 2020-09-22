177f033a4bc9fc57bb8c5d26f8238082a0ce3f16
dhiraj-deb-au7 <dhirajdeb@hotmail.com>
2020-09-22 16:53:59 +0530

    Edge-case adjustment

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -11,14 +11,13 @@ const userController = {
         if (await comparePassword(req.body.password, user.password)) {
           const token = jwt.sign({ _id: user._id }, process.env.SECRET_KEY);
           const { _id, firstName, lastName, email } = user;
-          res.json({ token, _id, firstName, lastName, email, _id });
-          console.log(res.json);
+          res.status(200).send({ token, firstName, lastName, email, _id });
         } else {
-          res.status(400).send("password did not match!");
+          res.status(400).send("Password did not match!");
         }
       } else {
-        res.status(404).send("user not found! please signup first...");
-      } 
+        res.status(404).send("User not found! Please signup first...");
+      }
     } catch (error) {
       console.log(error);
       res.status(400).send(error._message);
@@ -28,7 +27,7 @@ const userController = {
   signup: async (req, res) => {
     try {
       const user = await User.signup(req.body);
-      res.status(200).send("user created successfully!");
+      res.status(200).send("User created successfully!");
     } catch (error) {
       console.log(error);
       res.status(400).send(error._message);
