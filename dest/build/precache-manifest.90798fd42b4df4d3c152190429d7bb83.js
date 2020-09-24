e9e8feea773375623e942161fa2f73e19074a7d9
dhiraj-deb-au7 <dhirajdeb@hotmail.com>
2020-09-24 18:45:20 +0530

    Heroku deployment test

---
+++ dest/build/precache-manifest.90798fd42b4df4d3c152190429d7bb83.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6758fd2aabf378f87c2e9e782334d6c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46541effe4df0188b62d",
+    "url": "/static/css/main.f2cece28.chunk.css"
+  },
+  {
+    "revision": "84be6e97e10872c985d6",
+    "url": "/static/js/2.8aa19888.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.8aa19888.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46541effe4df0188b62d",
+    "url": "/static/js/main.48f23ac4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
