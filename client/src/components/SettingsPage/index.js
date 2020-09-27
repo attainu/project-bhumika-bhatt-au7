56874f776ead26b1204e4ce1f212731a0c659b25
dhiraj-deb-au7 <dhirajdeb@hotmail.com>
2020-09-27 23:46:50 +0530

    Settings-FE done

--- client/src/components/SettingsPage/index.js
+++ client/src/components/SettingsPage/index.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Row, Col, Card, CardTitle, TextInput } from "react-materialize";
+import {
+  Row,
+  Col,
+  Card,
+  CardTitle,
+  TextInput,
+  Container,
+  Button,
+} from "react-materialize";
 
 import style from "./SettingsPage.module.css";
 
@@ -12,35 +20,90 @@ const SettingsPage = (props) => {
     userName,
     country,
     mobile,
+    inputHandler,
+    submitHandler,
+    firstNameRef,
+    lastNameRef,
+    userNameRef,
+    countryRef,
+    mobileRef,
+    changeName,
+    editName,
+    openForm,
   } = props;
   return (
-    <div>
-      <div className={style.profilePic}>
+    <Container>
+      <Container>
         <Row>
-          <Col>
+          <Col s={10} l={4} m={6} offset="s1 m3 l4">
             <Card
               header={
-                <CardTitle image="https://materializecss.com/images/sample-1.jpg"></CardTitle>
+                <CardTitle image="https://www.w3schools.com/w3images/avatar2.png"></CardTitle>
               }
-              title={firstName + " " + lastName}
-            ></Card>
+            >
+              {firstName + " " + lastName + " "}
+
+              <a href="#" onClick={openForm}>
+                Edit
+              </a>
+            </Card>
+            {editName && (
+              <form onSubmit={changeName} className={style.nameForm}>
+                <TextInput
+                  id="firstName"
+                  name="firstName"
+                  value={firstName}
+                  label="First name"
+                  onChange={inputHandler}
+                  ref={firstNameRef}
+                  noLayout
+                />
+                <TextInput
+                  id="lastName"
+                  name="lastName"
+                  value={lastName}
+                  label="Last Name"
+                  onChange={inputHandler}
+                  ref={lastNameRef}
+                  noLayout
+                />
+                <Button small>Change name</Button>
+              </form>
+            )}
           </Col>
         </Row>
-      </div>
-      <div className="container">
-        <TextInput
-          id="TextInput-4"
-          value={email}
-          label="Email"
-          error={error}
-          success="Email successfully changed!"
-          validate
-        />
-        <TextInput id="TextInput-4" value={userName} label="Username" />
-        <TextInput id="TextInput-4" value={country} label="Country" />
-        <TextInput id="TextInput-4" value={mobile} label="Mobile" />
-      </div>
-    </div>
+      </Container>
+      <Container>
+        <form onSubmit={submitHandler}>
+          <TextInput id="email" value={email} label="Email" readOnly />
+          <TextInput
+            id="username"
+            name="userName"
+            value={userName}
+            label="Username"
+            onChange={inputHandler}
+            ref={userNameRef}
+          />
+          <TextInput
+            id="country"
+            name="country"
+            value={country}
+            label="Country"
+            onChange={inputHandler}
+            ref={countryRef}
+          />
+          <TextInput
+            id="mobile"
+            name="mobile"
+            value={mobile}
+            label="Mobile"
+            onChange={inputHandler}
+            ref={mobileRef}
+          />
+          <Button type="submit">Save</Button>
+        </form>
+      </Container>
+    </Container>
   );
 };
 
--- client/src/containers/settings/index.js
+++ client/src/containers/settings/index.js
@@ -1,34 +1,142 @@
 import React, { Component } from "react";
+import axios from "axios";
+import M from "materialize-css";
 
 import { SettingsPage } from "../../components";
+import { WEB_URL } from "../../configs";
 
 class Settings extends Component {
-  localStorageData = localStorage.getItem("User");
+  localStorageData = JSON.parse(localStorage.getItem("User"));
 
   state = {
     user: {
-      firstName: "",
-      lastName: "",
-      email: "",
-      mobile: "",
-      userName: "",
-      country: "",
+      id: this.localStorageData._id,
+      firstName: this.localStorageData.firstName,
+      lastName: this.localStorageData.lastName,
+      email: this.localStorageData.email,
+      mobile: this.localStorageData.mobile,
+      userName: this.localStorageData.userName,
+      country: this.localStorageData.country,
     },
+    editName: false,
+  };
+
+  firstNameRef = React.createRef();
+  lastNameRef = React.createRef();
+  userNameRef = React.createRef();
+  countryRef = React.createRef();
+  mobileRef = React.createRef();
+
+  componentDidMount() {
+    this.getUserProfile();
+  }
+
+  getUserProfile = async (e) => {
+    try {
+      const response = await axios.get(
+        `profile/api/v2/${localStorage.getItem("id")}`
+      );
+      console.log(response.data);
+
+      if (response) {
+        this.setState({
+          user: {
+            id: response.data._id,
+            firstName: response.data.firstName,
+            lastName: response.data.lastName,
+            email: response.data.email,
+            mobile: response.data.mobile,
+            userName: response.data.userName,
+            country: response.data.country,
+          },
+        });
+
+        localStorage.setItem("User", JSON.stringify(response.data));
+      }
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   inputHandler = (e) => {
     const name = e.target.name;
     const value = e.target.value;
-    const { signupFormData, loginFormData } = this.state;
+    const { user } = this.state;
     this.setState({
       error: "",
-      signupFormData: { ...signupFormData, [name]: value },
-      loginFormData: { ...loginFormData, [name]: value },
+      user: { ...user, [name]: value },
     });
   };
 
+  openForm = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      editName: true,
+    });
+  };
+
+  changeName = async (e) => {
+    e.preventDefault();
+
+    const { user } = this.state;
+    const { history } = this.props;
+
+    try {
+      const response = await axios.patch("/profile/api/v3", {
+        _id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userName: user.userName,
+        country: user.country,
+        mobile: user.mobile,
+      });
+
+      if (response) {
+        M.toast({ html: response.data });
+        this.setState({
+          editName: false,
+        });
+        history.push(WEB_URL.SETTINGS);
+        console.log(response.data);
+      }
+    } catch (error) {
+      M.toast({ html: error.response });
+      console.log("Error:", error.response);
+    }
+  };
+
+  submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const { user } = this.state;
+      const { history } = this.props;
+      console.log(user);
+
+      const response = await axios.patch("/profile/api/v3", {
+        _id: user.id,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        userName: user.userName,
+        country: user.country,
+        mobile: user.mobile,
+      });
+
+      if (response) {
+        M.toast({ html: response.data });
+        history.push(WEB_URL.SETTINGS);
+        console.log(response.data);
+      }
+    } catch (error) {
+      M.toast({ html: error.response });
+      console.log("Error:", error.response);
+    }
+  };
+
   render() {
-    const user = JSON.parse(localStorage.getItem("User"));
+    const { user, editName } = this.state;
+
     return (
       <SettingsPage
         firstName={user.firstName}
@@ -37,6 +145,16 @@ class Settings extends Component {
         userName={user.userName}
         country={user.country}
         mobile={user.mobile}
+        submitHandler={this.submitHandler}
+        inputHandler={this.inputHandler}
+        firstNameRef={this.firstNameRef}
+        lastNameRef={this.lastNameRef}
+        userNameRef={this.userNameRef}
+        countryRef={this.countryRef}
+        mobileRef={this.mobileRef}
+        editName={editName}
+        changeName={this.changeName}
+        openForm={this.openForm}
       />
     );
   }
