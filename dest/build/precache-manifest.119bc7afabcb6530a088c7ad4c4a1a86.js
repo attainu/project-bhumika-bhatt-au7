215b055a5cc7615016acd11b7240fbcd63c116c7
dhiraj-deb-au7 <dhirajdeb@hotmail.com>
2020-10-08 20:40:20 +0530

    Deployment

---
+++ dest/build/precache-manifest.119bc7afabcb6530a088c7ad4c4a1a86.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ab7a9de7f321ed5d9027d6ad1bcc98",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49e6df86580285bea398",
+    "url": "/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "6007f33320251bf8afdc",
+    "url": "/static/css/main.216e6ebe.chunk.css"
+  },
+  {
+    "revision": "49e6df86580285bea398",
+    "url": "/static/js/2.674edac0.chunk.js"
+  },
+  {
+    "revision": "cec4ede2e9e059ccb444406734ab4ea2",
+    "url": "/static/js/2.674edac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6007f33320251bf8afdc",
+    "url": "/static/js/main.599d8d01.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
